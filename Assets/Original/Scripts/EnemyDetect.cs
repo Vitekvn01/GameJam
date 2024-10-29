e14fb159226af12d8437732e3d8fcb7f6c2d697e
BoldElf <denis.den.kovalenko11@mail.ru>
2024-10-29 15:25:15 +0300

    Add enemyDetect.

--- Assets/Original/Scripts/EnemyDetect.cs
+++ Assets/Original/Scripts/EnemyDetect.cs
@@ -7,48 +7,31 @@ using static UnityEditor.PlayerSettings;
 public class EnemyDetect : MonoBehaviour
 {
     [SerializeField] private GameObject player;
-    //[SerializeField] private int distance = 33;
+    [SerializeField] private int distance = 33;
 
-    // Заметил ли бот врага.
-    private bool angry = false;
-
-    // Для сброса режима.
-    private float timer; 
-
-    // Для проверки зоны сбора, которая мешает рейкасту.
-    private Interaction interaction;
-    private CapsuleCollider capsuleCollider;
-
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if(other.gameObject == player)
-        {
-            angry = true;
-        }
-    }
+    private int layer = ~0; // Проверка всех слоев.
+    private bool angry;     // Переход в режим преследования.
 
     private void Update()
     {
-        /*
         RaycastHit hit;
         // Рейкаст для отслеживания.
-        if (Physics.Raycast(gameObject.transform.position, Vector3.forward, out hit, distance))
+        Ray ray = new Ray(gameObject.transform.position, transform.forward);
+
+        // Проверка попадания
+        if (Physics.Raycast(ray, out hit, distance, layer, QueryTriggerInteraction.Ignore))
         {
-            Debug.Log(hit.collider.gameObject);
+            Debug.Log(hit.collider.gameObject);                 // DBG
 
             // Если заметили игрока -> переход в злой режим.
-            if(hit.collider.gameObject == player)
+            if (hit.collider.gameObject == player)
             {
-                angry = true; 
+                angry = true;
             }
-          
+
+
+            Debug.DrawLine(ray.origin, hit.point, Color.red);   // DBG
+            Debug.Log(angry);                                   // DBG
         }
-
-        Debug.Log(angry);
-
-        */
-        Debug.Log(angry);
     }
-
 }
