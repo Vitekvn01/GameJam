fc7e77e3f8bb88e461d202e190b67fda220b3090
BoldElf <denis.den.kovalenko11@mail.ru>
2024-11-01 14:35:03 +0300

    Settings

--- Assets/Original/Scripts/Quests/QuestPerson.cs
+++ Assets/Original/Scripts/Quests/QuestPerson.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class QuestPerson : MonoBehaviour, IQuestPerson
 {
+    [SerializeField] private bool endDialog = false;
 
-
+    [SerializeField] private GameObject canvasObject;
 
     public void dialog()
     {
+        if (endDialog == true)
+        {
+            canvasObject.SetActive(true);
+            return;
+        }
+
         if (GetComponent<SubtitlesContainer>() != null)
         {
             GetComponent<SubtitlesContainer>().PlaySubtitlesContainer();
